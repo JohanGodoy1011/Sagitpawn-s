1dc979ff986da394f66658acf6c399b3680a0792
JorgeMS05 <32069830+JorgeMS05@users.noreply.github.com>
2017-10-24 00:05:09 +0200

    Nueva gestión de disparo
    Se ha añadido una segunda versión de código que permite generar una trayectoria en bitmap sobre la bala al dispararla. Hemos de profundizar en ella.

--- Sagitpawn's Code - Fase 1/script.js
+++ Sagitpawn's Code - Fase 1/script.js
@@ -12,18 +12,20 @@ window.onload = function(){
         var jugador1;
         var jugador2;
 
+        /* Codigo disparo v1
         // Creamos una variable grupal para los proyectiles: en proceso
         var ball;
         var ball1;
         var ball2;
-        
+        */
     
-        var suelo;
+        var suelo;/*
         var arrow;
         var catchFlag = false;
         var bolaON = false;
         var launchVelocity = 0;
-    
+        */
+
         var barConfig;
         var barraJ1;
         var barraJ2;
@@ -33,6 +35,19 @@ window.onload = function(){
         var hit = false;
         var diftime = 0;
         var elapsed = 0;
+
+
+        // Sistema de disparos versión dos: desprototipado loco
+        var gun;
+        var SHOT_DELAY;
+        var BULLET_SPEED;
+        var GRAVITY;
+        //var NUMBER_OF_BULLETS;  -> sustituido por 1
+        var bulletPool;
+        var timeOffset;
+        var lastBulletShotAt;
+        var bitmap;
+
 
         function preload() {
     
@@ -44,14 +59,19 @@ window.onload = function(){
             game.load.spritesheet('j1Disparando', 'Sprites/lanzerFire.png',55, 75, 20);             //Cargamos la animación del j1 para disparar 'fire'
             game.load.spritesheet('jugador2', 'Sprites/lanzer2.png', 55, 75, 20);                    //Cargamos el spritesheet del primer jugador, la que será la animación 'idle'.
             game.load.spritesheet('j2Cargando', 'Sprites/lanzer2Charging.png',55, 75, 8);
-            // Falta generar un nuevo modelo para el jugador 2, hasta entonces se empleará el spritesheet j1Disparando
-            // Preguntar duda acerca de como reacciona un spritesheet con:
-            // sprite.rotation = game.physics.arcade.angleToPointer(sprite);
 
             //Sprites para cargar las mierdas de las bolas
             game.load.image('arrow', 'Imagenes/longarrow2.png');
             game.load.image('ball', 'Imagenes/pangball.png');
             game.load.image('analog', 'Imagenes/fusia.png');
+
+
+            ////////////////////////
+            // Disparos versión 2
+            game.load.image('bullet', 'bullet.png');
+            game.load.image('ground', 'ground.png');
+            game.load.image('explosion', 'explosion.png', 128, 128);
+
     
         }
     
@@ -75,9 +95,9 @@ window.onload = function(){
             // Generamos el suelo(perteneciente a sólidos)
             suelo = solidos.create(0, game.world.height - 80, 'suelo');
             //suelo.scale.setTo(1, 1);                               //Escalamos la plataforma, (no se como funciona xd)
-            suelo.body.immovable = true;                            //Para que la plataforma no se caiga al colisionar con ella
+            suelo.body.immovable = true;                             //Para que la plataforma no se caiga al colisionar con ella
     
-    
+            /*
             //Vamos con la bola:
             analog = game.add.sprite(400, 350, 'analog');
             
@@ -88,19 +108,7 @@ window.onload = function(){
             analog.rotation = 220;
             analog.alpha = 0;
             analog.anchor.setTo(0.5, 0.0);
-           
-            //Físicas sobre la flecha
-            arrow = game.add.sprite(400, 350, 'arrow');
-            game.physics.enable(arrow, Phaser.Physics.ARCADE);
-            
-            arrow.anchor.setTo(0.1, 0.5);
-            arrow.body.moves = false;
-            arrow.body.allowGravity = false;
-            arrow.alpha = 0;
-                
-            game.camera.follow(jugador1);
-    
-            
+            */      
 
             ///////////////////////////////
             // Gestión de jugadores:
@@ -121,16 +129,25 @@ window.onload = function(){
     
             // Enable input para ambos jugadores
             jugador1.inputEnabled = true;
-            jugador1.events.onInputDown.add(set);                   //Genera el objeto ball
+            //Código de disparo v2
+            jugador1.events.onInputDown.add(charge);
+            jugador1.events.onDragStart.add(drawTraj);                  //Hay que llaamrlo en update de alguna forma
+            jugador1.events.onInputUp.add(shootBullet);         
+            //jugador1.events.onInputDown.add(set);                   //Genera el objeto ball
             jugador1.events.onInputDown.add(setSprite);             //Cambia el spritesheet
-            jugador1.events.onInputUp.add(launchBall);              //Dispara la bola y cambia nuevamente el spritesheet
-    
+            //jugador1.events.onInputUp.add(launchBall);              //Dispara la bola y cambia nuevamente el spritesheet
+            jugador1.events.onInputUp.add(clean);
+
+
             jugador2.inputEnabled = true;
-            jugador2.events.onInputDown.add(set);
+            jugador2.events.onInputDown.add(charge);
+            jugador2.events.onDragStart.add(drawTraj);                  //Hay que llaamrlo en update de alguna forma
+            jugador2.events.onInputUp.add(shootBullet);
+            //jugador2.events.onInputDown.add(set);
             jugador2.events.onInputDown.add(setSprite);                        
-            jugador2.events.onInputUp.add(launchBall);
+            //jugador2.events.onInputUp.add(launchBall);
 
-
+            game.camera.follow(jugador1);
             //////////////////////////////////////////////////
             ///////////////// Barra de Vida///////////////////
             //////////////////////////////////////////////////
@@ -145,11 +162,226 @@ window.onload = function(){
 
             barraJ2 = new HealthBar(this.game, barConfig);
             barraJ2.setPosition(jugador2.x + 20, jugador2.y - 10);
+
+
+            /*
+            //Físicas sobre la flecha
+            arrow = game.add.sprite(player.x, player.y, 'arrow');
+            game.physics.enable(arrow, Phaser.Physics.ARCADE);
+            
+            //arrow.anchor.setTo(0.1, 0.5);
+            arrow.body.moves = false;
+            arrow.body.allowGravity = false;
+            arrow.alpha = 0;
+            */
+
+
+
+            ////////////////////////////////////////  Código Disparo v2 ////////////////////////////////////////////
+            /*
+            // Define constants
+            SHOT_DELAY = 300; // milliseconds (10 bullets/3 seconds)
+            BULLET_SPEED = 800; // pixels/second
+            GRAVITY = 980; // pixels/second/second
+
+            gun = game.add.sprite(jugador1.x, jugador1.y, 'bullet');
+            //gun.anchor.setTo(0.5, 0.5);
+
+            bulletPool = game.add.group();
+            
+            bulletPool.enableBody = true;
+            
+            for(var i = 0; i < 1; i++) {
+                // Create each bullet and add it to the group.
+                
+                var bullet = bulletPool.create(0, 0, 'bullet');
+        
+                // Set its pivot point to the center of the bullet
+                bullet.anchor.setTo(0.5, 0.5);
+        
+                // Enable physics on the bullet
+                //game.physics.enable(bullet, Phaser.Physics.ARCADE);
+        
+                // Set its initial state to "dead".
+                bullet.kill();
+            }
+
+            // Esto lo aplica al bitmap
+            bullet.body.gravity.y = GRAVITY;
+            //
+            //gun.body.gravity.y = GRAVITY;
+
+
+            bitmap = game.add.bitmapData(game.width, game.height);
+            bitmap.context.fillStyle = 'rgb(255, 255, 255)';
+            bitmap.context.strokeStyle = 'rgb(255, 255, 255)';
+            game.add.image(0, 0, bitmap);
+
+
+            game.input.activePointer.x = game.width/2;
+            game.input.activePointer.y = game.height/2 - 100;*/
+            /////////////////////////////////////////////////////////////
+            bitmap = game.add.bitmapData(game.width, game.height);
+            gun = game.add.sprite(jugador1.x, jugador1.y, 'bullet');
+            bulletPool = game.add.group();
+            bulletPool.enableBody = true;
             
         }
     
-        
 
+        function charge(){
+            
+               // Define constants
+               SHOT_DELAY = 300; // milliseconds (10 bullets/3 seconds)
+               BULLET_SPEED = 800; // pixels/second
+               GRAVITY = 980; // pixels/second/second
+   
+               //gun = game.add.sprite(jugador1.x, jugador1.y, 'bullet');
+               //gun.anchor.setTo(0.5, 0.5);
+   
+               //bulletPool = game.add.group();
+               //bulletPool.enableBody = true;
+               
+               for(var i = 0; i < 1; i++) {
+                   // Create each bullet and add it to the group.
+                   /*
+                   var bullet = game.add.sprite(0, 0, 'bullet');
+                   bulletPool.add(bullet);*/
+                   var bullet = bulletPool.create(0, 0, 'bullet');
+           
+                   // Set its pivot point to the center of the bullet
+                   bullet.anchor.setTo(0.5, 0.5);
+           
+                   // Enable physics on the bullet
+                   //game.physics.enable(bullet, Phaser.Physics.ARCADE);
+           
+                   // Set its initial state to "dead".
+                   bullet.kill();
+               }
+   
+               // Esto lo aplica al bitmap
+               bullet.body.gravity.y = GRAVITY;
+               //
+               //gun.body.gravity.y = GRAVITY;
+   
+   
+               bitmap = game.add.bitmapData(game.width, game.height);
+               bitmap.context.fillStyle = 'rgb(255, 255, 255)';
+               bitmap.context.strokeStyle = 'rgb(255, 255, 255)';
+               game.add.image(0, 0, bitmap);
+   
+   
+               game.input.activePointer.x = game.width/2;
+               game.input.activePointer.y = game.height/2 - 100;
+
+
+        }
+
+
+        
+        // Función disparos v2. dibujado de trayectoria
+        function drawTraj(){
+            bitmap.context.clearRect(0, 0, game.width, game.height);
+            bitmap.context.fillStyle = 'rgba(255, 255, 255, 0.5)';
+            var MARCH_SPEED = 10; // Smaller is faster
+            timeOffset = timeOffset + 1 || 0;
+            timeOffset = timeOffset % MARCH_SPEED;
+
+            var correctionFactor = 0.99;
+            var theta = -gun.rotation;
+            var x = 0;
+            var y = 0;
+            for(var t = 0 + timeOffset/(1000*MARCH_SPEED/60); t < 3; t += 0.03) {
+                x = BULLET_SPEED * t * Math.cos(theta) * correctionFactor;
+                y = BULLET_SPEED * t * Math.sin(theta) * correctionFactor - 0.5 * GRAVITY * t * t;
+                bitmap.context.fillRect(x + player.x, player.y - y, 3, 3);
+                if (y < -15) 
+                break;
+            }
+        
+            bitmap.dirty = true;
+
+        }
+
+
+        function shootBullet(pointer){
+            if (lastBulletShotAt === undefined) {
+                console.log("Juaja time");
+                lastBulletShotAt = 0;
+            }
+            if (game.time.now - lastBulletShotAt < SHOT_DELAY) 
+                return;
+            lastBulletShotAt = game.time.now;
+
+            var bullet = bulletPool.getFirstDead();
+            if (bullet === null || bullet === undefined) 
+            return;
+            
+            bullet.revive();
+
+            bullet.checkWorldBounds = true;
+            bullet.outOfBoundsKill = true;
+        
+            // Set the bullet position to the gun position.
+            bullet.reset(gun.x, gun.y);
+            bullet.rotation = gun.rotation;
+        
+            // Shoot it in the right direction
+            bullet.body.velocity.x = Math.cos(bullet.rotation) * BULLET_SPEED;
+            bullet.body.velocity.y = Math.sin(bullet.rotation) * BULLET_SPEED;
+            bitmap.context.clearRect(0, 0, game.width, game.height);
+
+            // Cambio de sprites
+            if (pointer == jugador1){
+                idleJ1();
+            } else if(pointer == jugador2){
+                idleJ2();
+            }
+
+            
+        }
+
+        function clean(){
+            bitmap.context.clearRect(0, 0, game.width, game.height);
+        }
+
+        function update(){
+            
+            drawTraj();
+
+            //Variable para detectar colisión jugador-solidos
+            var hitPlatform = game.physics.arcade.collide(player, solidos);
+            //Variable para detectar colisión pelota-jugador:
+            //var hitJugador = game.physics.arcade.collide(player, ball);
+            //Variable para detectar colisión pelota-solidos:
+            //var hitBall = game.physics.arcade.collide(ball, solidos);
+
+            game.physics.arcade.collide(bulletPool, function(bullet) {
+                // Create an explosion
+                //this.getExplosion(bullet.x, bullet.y);
+        
+                // Kill the bullet
+                bullet.kill();
+            }, null, this);
+
+            bulletPool.forEachAlive(function(bullet) {
+                bullet.rotation = Math.atan2(bullet.body.velocity.y, bullet.body.velocity.x);
+            }, this);
+
+            gun.rotation = game.physics.arcade.angleToPointer(gun);
+            //bitmap.context.clearRect(0, 0, game.width, game.height);
+            
+
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+        /////////////////////////////////  Código Disparo v1 ///////////////////////////////
+
+        /*
         // Esta función genera un cuerpo ball (el proyectil)
         function set(ball) {
             ball.body.moves = false;
@@ -157,7 +389,7 @@ window.onload = function(){
             ball.body.allowGravity = true;
             catchFlag = true; 
         }
-
+*/
         // Con el parámetro pointer sabemos donde clicamos y alteramos el spritesheet
         function setSprite(pointer) {
             if (pointer == jugador1){
@@ -168,7 +400,7 @@ window.onload = function(){
                 chargeJ2();
             }
         }
-    
+    /*
         // El cuerpo ball creado en set se genera en el lugar donde se clica gracias a la info que nos da el parámetro pointer  
         function launchBall(pointer) {
     
@@ -199,8 +431,8 @@ window.onload = function(){
             ball.body.velocity.setTo(Xvector, Yvector);
     
         }
-    
-    
+        
+    */
         //////////////////////////////////////////////////////
         // ///////Sistema de control de spritesheets j1://///
         /////////////////////////////////////////////////////
@@ -210,13 +442,13 @@ window.onload = function(){
             jugador1.animations.add('charging');
             jugador1.animations.play('charging', 8, true);
         }
-        /*
+        
         function fireJ1(){
             jugador1.loadTexture('j1Disparando', 0);
             jugador1.animations.add('fire');
             jugador1.animations.play('fire', 8, true);
         }
-        */
+        
         function idleJ1(){
             jugador1.loadTexture('jugador1', 0);
             jugador1.animations.add('idle');
@@ -229,23 +461,22 @@ window.onload = function(){
             jugador2.animations.add('charging');
             jugador2.animations.play('charging', 8, true);
         }
-        /*
+        
         function fireJ2(){
             jugador2.loadTexture('j1Disparando', 0);
             jugador2.animations.add('fire');
             jugador2.animations.play('fire', 8, true);
         }
-        */
+        
         function idleJ2(){
             jugador2.loadTexture('jugador2', 0);
             jugador2.animations.add('idle');
             jugador2.animations.play('idle', 8, true);
         }
-    
+    /*
         ///////////////////////////////////////////////////////
-        //////Gestión vida y puntuación///////////////1///////
+        //////Gestión vida y puntuación////////////////////////
         ///////////////////////////////////////////////////////
-
 
         function colisionPelotaJ1(player, ball) {
             vidaJ1 -= 20;
@@ -256,7 +487,8 @@ window.onload = function(){
             vidaJ2 -= 20;
             barraJ2.setPercent(vidaJ2);
         }
-    
+
+
         function update() {
             game.physics.arcade.overlap(jugador1, ball, colisionPelotaJ1, null, this);
             game.physics.arcade.overlap(jugador2, ball, colisionPelotaJ2, null, this);
@@ -317,7 +549,8 @@ window.onload = function(){
             }
 
             //Comprobaciones por si se ha acabado el juego
-            if (vidaJ1 == 0 || vidaJ2 == 0){ /*Finalizamos el juego*/}
+            if (vidaJ1 == 0 || vidaJ2 == 0){ /*Finalizamos el juego}
     
-        }
+        }*/
+        
     }
